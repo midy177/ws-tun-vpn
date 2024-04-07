2389d9effcc7cba388a83a78703c8c14d3363a38
Your Name <your@example.com>
2024-04-07 18:00:50 +0800

    add container.NewPadded

--- wtvc_gui/pages/page_content.go
+++ wtvc_gui/pages/page_content.go
@@ -1,6 +1,7 @@
 package pages
 
 import (
+	"fyne.io/fyne/v2/container"
 	"ws-tun-vpn/wtvc_gui/app_context"
 )
 
@@ -8,13 +9,13 @@ func MakePageContentLoop(ctx *app_context.AppContext) {
 	for navEvent := range ctx.NavChannel {
 		switch navEvent.TargetPage {
 		case app_context.MainPage:
-			ctx.Window.SetContent(MakeMainPageContent(ctx))
+			ctx.Window.SetContent(container.NewPadded(MakeMainPageContent(ctx)))
 		case app_context.AddConfigPage:
-			ctx.Window.SetContent(MakeConfigsPage(ctx, true))
+			ctx.Window.SetContent(container.NewPadded(MakeConfigsPage(ctx, true)))
 		case app_context.ModifyConfigPage:
-			ctx.Window.SetContent(MakeConfigsPage(ctx, false))
+			ctx.Window.SetContent(container.NewPadded(MakeConfigsPage(ctx, false)))
 		case app_context.ConnectTo:
-			ctx.Window.SetContent(MakeConnectPage(ctx))
+			ctx.Window.SetContent(container.NewPadded(MakeConnectPage(ctx)))
 		}
 	}
 }
